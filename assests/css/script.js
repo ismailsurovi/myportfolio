daa70ed55b88ec15e7919ba526b73f180ef43d74
ismailsurovi <contact.mdismail@gmail.com>
2023-09-23 21:53:35 +0600

    myportfolio

---
+++ assests/css/script.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+  $('.slick_slider').slick({
+    //setting-name: setting-value
+     slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    responsive: [
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+  ]
+  });
+
+  $('.slick_slider_testimonial').slick({
+    //setting-name: setting-value
+     slidesToShow: 2,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    responsive: [
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+  ]
+  });
+
+
+
+});
